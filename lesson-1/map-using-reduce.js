030058364e6f5ab6dd146d72d202d06de94d457e
tylermcgraw <=>
2024-11-12 20:21:53 -0500

    map using reduce

---
+++ lesson-1/map-using-reduce.js
@@ -0,0 +1,16 @@
+function map(array, callback, thisArg) {
+  return array.reduce((accumulator, value, index, array) => {
+    accumulator.push(callback(value, index, array));
+    return accumulator;
+  }, [], thisArg);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
+console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
+console.log(map(numbers, () => false));
+// => [ false, false, false, false, false ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(map(values, value => String(value)));
+// => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
